f79dfa5ae367dfcab116e68c92c511cff59bf7e0
as6325400 <as6325400@gmail.com>
2023-09-12 14:42:09 +0800

    complete

--- main.py
+++ main.py
@@ -2,38 +2,28 @@ import requests
 from bs4 import BeautifulSoup as bs
 from tool import login
 from tqdm import tqdm
+from env import ROBCLASSLIST
 
 Robclass = False
 count = 0
 
+classList = ROBCLASSLIST
+
 while True:
     SESSIONID = login.getSession()
     count += 1
+    print(count, end=" ")
     for i in tqdm(range(1, 150 + 1)):
-        paylaod = {"session_id": SESSIONID,
-                "dept" : "4104",
-                "grade" : "3", 
-                "e": "0",
-                "m": "0",
-                "SelectTag": "1",
-                "4103001_01": "1",
-                "4103001_02": "1",
-                "4103022_01": "2",
-                "course": "4103022_01",
-                "4103026_01": "2",
-                "4103055_01": "1",
-                "4103055_02": "1",
-                "4103800_01": "1"
-                }
-        
+        paylaod = classList[i % len(classList)]
+        paylaod['session_id'] = SESSIONID
         response = requests.post("http://140.123.30.101/~ccmisp98/cgi-bin/class/Add_Course01.cgi", paylaod)
         response.encoding = 'UTF-8'
         val = bs(response.text, features='html.parser')
         ans = str(val.find_all('font'))
         if "錯誤" not in ans:
-            Robclass = True
-            break
-    if Robclass == True:
-        print("Success")
-        print(f"Use {count} Rounds and {i} Times")
+            print("Success")
+            print(f"Use {count} Rounds and {i} Times")
+            classList.pop(i % len(classList))
+    print()
+    if  len(classList) == 0:
         break
