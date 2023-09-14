6c717515251bf86b28ebc0d2281a01848c202e8d
as6325400 <as6325400@gmail.com>
2023-09-14 14:12:21 +0800

    add log

--- main.py
+++ main.py
@@ -18,25 +18,27 @@ import requests
 from bs4 import BeautifulSoup as bs
 from time import sleep
 
-with open("log.txt", "a") as log_file:
-    count = 0
-    while True:
-        SESSIONID = login.getSession()
-        count += 1
-        log_file.write(f"{datetime.datetime.now()} {count}\n")
-        for i in tqdm(range(1, ROUNDTIMES + 1)):
-            sleep(1 / TIMESINONESECOND)
-            paylaod = classList[i % len(classList)]
-            paylaod['session_id'] = SESSIONID
-            response = requests.post(URL + ROBCLASSROUTER, paylaod)
-            response.encoding = 'UTF-8'
-            val = bs(response.text, features='html.parser')
-            ans = str(val.find_all('font'))
-            if "錯誤" not in ans:
+count = 0
+while True:
+    SESSIONID = login.getSession()
+    count += 1
+    with open("log.txt", "a") as log_file:
+        log_file.write(f"{datetime.datetime.now()} {count} Rounds\n")
+    for i in tqdm(range(1, ROUNDTIMES + 1)):
+        sleep(1 / TIMESINONESECOND)
+        paylaod = classList[i % len(classList)]
+        paylaod['session_id'] = SESSIONID
+        response = requests.post(URL + ROBCLASSROUTER, paylaod)
+        response.encoding = 'UTF-8'
+        val = bs(response.text, features='html.parser')
+        ans = str(val.find_all('font'))
+        if "錯誤" not in ans:
+            with open("log.txt", "a") as log_file:
                 log_file.write(f"Success to rob classID {classList[i % len(classList)]['course'][:-3]} on {datetime.datetime.now()}\n")
                 log_file.write(f"Use {count} Rounds and {i} Times\n")
-                classList.pop(i % len(classList))
-                if len(classList) == 0:
-                    break
-        if len(classList) == 0:
-            break
+            classList.pop(i % len(classList))
+            if len(classList) == 0:
+                break
+    if len(classList) == 0:
+        break
+
