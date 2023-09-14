3d83ab22b9d78c789dfb74ef12d9f864a0a8d2be
as6325400 <as6325400@gmail.com>
2023-09-14 11:19:46 +0800

    add log

--- main.py
+++ main.py
@@ -12,23 +12,31 @@ count = 0
 classList = ROBCLASSLIST
 
 
-while True:
-    SESSIONID = login.getSession()
-    # print(SESSIONID)
-    count += 1
-    print(count, end=" ")
-    for i in tqdm(range(1, ROUNDTIMES + 1)):
-        sleep(1 / TIMESINONESECOND)
-        paylaod = classList[i % len(classList)]
-        paylaod['session_id'] = SESSIONID
-        response = requests.post(URL + ROBCLASSROUTER, paylaod)
-        response.encoding = 'UTF-8'
-        val = bs(response.text, features='html.parser')
-        ans = str(val.find_all('font'))
-        if "錯誤" not in ans:
-            print(f"Success to rob classID {classList[i % len(classList)]['course'][:-3]} on {datetime.datetime.now()}")
-            print(f"Use {count} Rounds and {i} Times")
-            classList.pop(i % len(classList))
-    print()
-    if  len(classList) == 0:
-        break
+import datetime
+from tqdm import tqdm
+import requests
+from bs4 import BeautifulSoup as bs
+from time import sleep
+
+with open("log.txt", "a") as log_file:
+    count = 0
+    while True:
+        SESSIONID = login.getSession()
+        count += 1
+        log_file.write(f"{datetime.datetime.now()} {count}\n")
+        for i in tqdm(range(1, ROUNDTIMES + 1)):
+            sleep(1 / TIMESINONESECOND)
+            paylaod = classList[i % len(classList)]
+            paylaod['session_id'] = SESSIONID
+            response = requests.post(URL + ROBCLASSROUTER, paylaod)
+            response.encoding = 'UTF-8'
+            val = bs(response.text, features='html.parser')
+            ans = str(val.find_all('font'))
+            if "錯誤" not in ans:
+                log_file.write(f"Success to rob classID {classList[i % len(classList)]['course'][:-3]} on {datetime.datetime.now()}\n")
+                log_file.write(f"Use {count} Rounds and {i} Times\n")
+                classList.pop(i % len(classList))
+                if len(classList) == 0:
+                    break
+        if len(classList) == 0:
+            break
